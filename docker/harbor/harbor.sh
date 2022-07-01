2104cc9fef425825c7a615742f4980b7a2e1f308
leo <498242682@qq.com>
2022-07-01 11:36:56 +0800

    init

--- docker/harbor/harbor.sh
+++ docker/harbor/harbor.sh
@@ -8,4 +8,5 @@ tar -zxvf harbor-offline-installer-v2.5.1.tgz
 cd harbor
 cp harbor.yml.tmpl harbor.yml
 echo "注释掉htps的配置内容，配置htp相关的参数，主要是hostname，port，其他都可以不用动。"
+echo "if your want use host = reg.myharbor.com , plaease vim your hosts (ubuntu:/etc/hosts)"
 echo "then run harbor2.sh"
